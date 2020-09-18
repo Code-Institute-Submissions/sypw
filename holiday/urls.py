c35dd149195634fca1562ef2746d3233bfc5c04c
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-18 17:44:16 +0000

    Add internal menu on 'holidays' page

--- holiday/urls.py
+++ holiday/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.holiday, name='holiday')
+    path('', views.holiday, name='holiday'),
+    path('see/', views.see_holiday, name='see_holiday')
 ]
--- holiday/views.py
+++ holiday/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def holiday(request):
-    """ a view to show holidays page"""
+    """ a view to show 'book holiday' page"""
 
     return render(request, 'holiday/holiday.html')
+
+def see_holiday(request):
+    """ a view to see all holidays"""
+
+    return render(request, 'holiday/see_holiday.html')
