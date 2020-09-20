0fb9205535480eb898d19697199dd9f1769bedbb
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-20 13:54:10 +0000

    added product and payslip model and migrations done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Payslip(models.Model):
+    name = models.CharField(max_length=254)
+    ni = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+    def get_ni(self):
+        return self.ni
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    additional_info = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
