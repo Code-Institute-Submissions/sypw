18f05b2f2a16657537ce22a96f62c403a7f7c916
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-21 22:38:52 +0000

    added adding to bag functionality, bug detected

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ a view to return bag page """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    
+    return redirect( 'bag/bag.html')
