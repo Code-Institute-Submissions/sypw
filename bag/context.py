52cc8b8bab3ceb13c7a82ea9fe52ffd442a271cc
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-21 21:34:31 +0000

    test: fixed issue with the context in bag app

--- bag/context.py
+++ bag/context.py
@@ -1,14 +1,11 @@
 # from decimal import Decimal
 from django.conf import settings
-from . import product
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-
-    total = product.price
 
     context = {
         'bag_items': bag_items,
