bc58c2d3ccf07894322d9a51964d64955c94611e
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-21 11:45:40 +0000

    customized Admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product, Payslip
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "price",
+        "additional_info",
+    )
 
-admin.site.register(Product)
+    ordering = ("pk",)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Payslip)
