f4106761da5d9e32a59bdf92948ea3908c4081b6
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-26 21:18:32 +0000

    created checkout app models

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+from products.models import Product
 
 class Order(models.Model):
 
@@ -18,3 +19,34 @@ class Order(models.Model):
     country = models.CharField(max_length=40, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """Create unique order id """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """ This one is to override original save method, in order to set the order number if it has not been set already"""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super.save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'PK {self.product.pk} on order {self.order.order_number}'
