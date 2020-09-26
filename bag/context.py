d4b9ec65097af06d12d464399dabe500ac863e61
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-26 18:12:43 +0000

    bug in adding item to the bag context processor found and eliminated successfully

--- bag/context.py
+++ bag/context.py
@@ -1,5 +1,5 @@
 # from decimal import Decimal
-# from django.conf import settings
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
@@ -8,9 +8,9 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
-    bag = request.session.get('', {})
+    bag = request.session.get('bag', {})
 
-    for item_id in bag.items():
+    for item_id, quantity in bag.items():
 
         product = get_object_or_404(Product, pk=item_id)
         total = product.price
