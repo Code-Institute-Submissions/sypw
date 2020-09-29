c6125bcbec7a08ed0b98294f77c11c9d52fba40b
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-09-29 16:51:02 +0000

    added stripe elements js and changes in views

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.context import bag_contents
+
+import stripe
 
 
 def checkout(request):
-    bag = request.session.get('bag', {})
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HAOJBF6R4l66HuJq6QLE4N6sucQB7GLrLbjtZjSYYL7cvc5ZRZwCJkA8J5QXeqfgeKQDb89K7yqDa30MyrGILB000FcfKgasM',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
+
+    print(stripe_public_key)
 
     return render(request, template, context)
