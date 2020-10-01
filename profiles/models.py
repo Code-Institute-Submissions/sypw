368b8e8c4ae68b39c5249dabfba32352f46f6b62
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-01 17:01:51 +0000

    fixed bug with overwriting user's email while checkout

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,6 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_full_name = models.CharField(max_length=50, null=True, blank=True)
-    default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -24,11 +23,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """ Create or update user profile, just like the name describes"""
 
     if created:
         UserProfile.objects.create(user=instance)
-    # will just save profile for existing users
+    # Existing users: just save the profile
     instance.userprofile.save()
