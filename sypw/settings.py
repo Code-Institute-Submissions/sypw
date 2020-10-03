55338a7d078648cc36befe322ac4de30295d5e96
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-03 16:32:35 +0000

    Deployment: added variables to connect with AWS S3 bucket

--- sypw/settings.py
+++ sypw/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     # Other apps
     'schedule',
     'crispy_forms',
-
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -146,7 +146,6 @@ else:
     }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -188,6 +187,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Connect to AWS
+if 'USE_AWS' in os.environ:
+
+    # AWS Bucket Config:
+    AWS_STORAGE_BUCKET_NAME = 'sypw'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 STRIPE_CURRENCY = 'gbp'
