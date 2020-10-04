d1161ebeb6414a20ba79fbb84a1a3aab63c2829c
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-04 20:50:01 +0000

    fixed issue with site relocation after form submitting

--- comunicado/forms.py
+++ comunicado/forms.py
@@ -34,4 +34,27 @@ class CreateInForum(ModelForm):
 class CreateInDiscussion(ModelForm):
     class Meta:
         model = Discussion
-        fields = "__all__"
+        fields = ('forum', 'discuss', 'nick',)
+
+        # def __init__(self, *args, **kwargs):
+        #     """
+        #     Add placeholders and classes, remove auto-generated
+        #     labels and set autofocus on first field
+        #     """
+        # super().__init__(*args, **kwargs)
+        # placeholders = {
+        #     'forum': 'Choose Forum',
+        #     'discuss': 'Write what you think',
+        #     'nick': 'Your name',
+        # }
+
+        # self.fields['forum'].widget.attrs['autofocus'] = True
+
+        # for field in self.fields:
+        #     if self.fields[field].required:
+        #         placeholder = f'{placeholders[field]} *'
+        #     else:
+        #         placeholder = placeholders[field]
+        #     self.fields[field].widget.attrs['placeholder'] = placeholder
+        #     self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+        #     self.fields[field].label = False
--- comunicado/views.py
+++ comunicado/views.py
@@ -17,7 +17,6 @@ def comunicado(request):
         'forums': forums,
         'count': count,
         'discussions': discussions
-
     }
 
     return render(request, 'comunicado/comunicado.html', context)
@@ -29,7 +28,7 @@ def addInForum(request):
         form = CreateInForum(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(reverse('/comunicado.html'))
+            return redirect(reverse('comunicado'))
     context = {'form': form}
     return render(request, 'comunicado/addInForum.html', context)
 
@@ -41,7 +40,7 @@ def addInDiscussion(request):
         form = CreateInDiscussion(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/comunicado.html')
+            return redirect('comunicado')
     context = {
         'form': form,
         'nick': nick
