a780a822931446ea182a205277bd951140fe45d3
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-08 18:00:26 +0000

    Add one level of choice to indicate size of the company

--- products/views.py
+++ products/views.py
@@ -13,3 +13,9 @@ def products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def decide(request):
+    """ a view to return products page """
+
+    return render(request, 'products/decide.html')
