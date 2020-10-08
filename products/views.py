71c7e9a20a14104e5c399ddb198d77c75490b813
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-08 18:52:15 +0000

    added new homepage look for logged in users

--- products/views.py
+++ products/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 from .models import Product
 
-@login_required
+
 def products(request):
     """ a view to return products page """
 
