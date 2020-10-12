2f87b5576d25af608a43b8e9185317738096ebe2
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-12 20:59:48 +0000

    change of homepage background, scripts moved to base.js, fixed error with wrong links on homepage for unknown user

---
+++ static/scripts/base.js
@@ -0,0 +1,22 @@
+// To fit all JavaScript that need to be available across all pages
+
+$('.toast').toast('show');
+
+
+$('.btt-link').click(function(e) {
+		window.scrollTo(0,0)
+    });
+    
+
+$('#accordion{{forloop.counter}}').collapse({
+            toggle: true
+        });
+
+
+function notMine(){
+    alert("This is not my Youtube Channel, but it's my favourite composer.")};
+
+
+
+
+
