eaf9d0a98b8210cd1f7c6ff00ab30fbc15aca170
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-14 19:53:03 +0000

    Add form to holiday page

--- holiday/views.py
+++ holiday/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 
 def holiday(request):
@@ -6,7 +7,28 @@ def holiday(request):
 
     return render(request, 'holiday/holiday.html')
 
+
 def see_holiday(request):
     """ a view to see all holidays"""
+    if request.method == "POST":
+        message_name = request.POST['your-name']
+        message_phone = request.POST['your-phone']
+        message_email = request.POST['your-email']
+        start_date = request.POST['start-date']
+        end_date = request.POST['end-date']
+        message = request.POST['message']
 
-    return render(request, 'holiday/see_holiday.html')
+        # send an email function
+        send_mail(
+            message_name,
+            message_phone,
+            message_email,
+            message,
+            start_date,
+            end_date,
+            ["stan.kazovsky@yahoo.com"]
+        )
+
+        return render(request, 'holiday/see_holiday.html', {})
+    else:
+        return render(request, 'holiday/see_holiday.html')
