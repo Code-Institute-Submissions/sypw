6e6a6c657ffe7ba3f5c9034f86a74400a9373caf
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-15 13:24:04 +0000

    fix send_mail function to book holiday

--- holiday/views.py
+++ holiday/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
 
 
 def holiday(request):
@@ -11,34 +13,47 @@ def holiday(request):
 def see_holiday(request):
     """ a view to see all holidays"""
     if request.method == "POST":
-        message_name = request.POST['your-name']
+        message_name = str(request.POST['your-name'])
         message_phone = request.POST['your-phone']
         message_email = request.POST['your-email']
-        start_date = request.POST['start-date']
-        end_date = request.POST['end-date']
+        dates = str([
+            str(request.POST['start-date']),
+            str(request.POST['end-date']),
+        ])
+        # end_date = request.POST['end-date']
         message = request.POST['message']
-
-        # send an email function
-        send_mail(
-            'message_name', [
-                'message_phone',
-                'message',
-                'start_date',
-                'end_date',
-            ],
-            'message_email',
-            ["stan.kazovsky@yahoo.com"],
-            fail_silently=True,
-        )
+        content = " Phone: " + message_phone + "  " + message + " in dates: " + dates
 
         context = {
             'message_name': message_name,
             'message_phone': message_phone,
             'message_email': message_email,
-            'start_date': start_date,
-            'end_date': end_date,
+            # 'start_date': start_date,
+            # 'end_date': end_date,
+            'dates': dates,
             'message': message,
+            'content': content,
         }
+
+        subject = message_name,
+        body = content
+        from_email = message_email
+
+
+        # send an email function
+        send_mail(
+            subject,
+            body,
+            from_email,
+            ['stan.kazovski@yahoo.com', settings.DEFAULT_FROM_EMAIL, ],
+            fail_silently=False,)
+
+        # send_mail(
+        #     f"New message from {request.POST.get('message_name', 'message_email')}", # subject of the email, contains the user's email you can respond to
+        #     request.POST.get('start_date', 'end_date', ), # body of the email
+        #     settings.DEFAULT_FROM_EMAIL, # Some email you want all your messages to be from, e.g. contact@mysite.com
+        #     ['myemail@gmail.com'] # where you want the contact email to be sent
+        # )
 
         return render(request, 'holiday/see_holiday.html', context)
 
