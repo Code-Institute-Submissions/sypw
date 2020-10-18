d4e1865ab1b197e93b12c7e2fd3ad65b07739997
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-10-18 17:55:13 +0000

    more style changes in forum part

--- comunicado/forms.py
+++ comunicado/forms.py
@@ -37,6 +37,28 @@ class CreateInDiscussion(ModelForm):
         forum = Forum.id
         fields = ['forum', 'discuss', 'nick', ]
 
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'forum': 'Choose Forum',
+            'discuss': 'Your message',
+            'nick': 'Your Name',
+        }
+
+        self.fields['discuss'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
+
 
 # class EditInDiscussion(ModelForm):
 #     class Meta:
